56d0c5517f104c66714a2c161c1c120bc3835702
Rip Britton <ripbritton3@gmail.com>
2023-11-18 09:36:37 -0600

    filtered results

--- src/components/results/Results.tsx
+++ src/components/results/Results.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Results.module.css';
 import { Card } from "../../models/Card";
 import CardImage from "../card-image/CardImage";
@@ -10,12 +10,24 @@ interface ResultsProps {
 }
 
 const Results: React.FC<ResultsProps> = ({ results, loading }) => {
+    const [showEncounter, setShowEncounter] = useState<boolean | null>(null);
+    // Filtered results based on the toggle state
+    const filteredResults = results.filter(card => {
+        if (showEncounter === null) return true; // Show all if no filter is selected
+        return showEncounter ? card.Classification === "Encounter" : card.Classification !== "Encounter";
+    });
+
 
     if (results.length > 0 || loading) {
         return (
             <div className={styles.resultsContainer}>
+                <div className={styles.filterButtons}>
+                    <button className={showEncounter === null ? styles.activeFilter : styles.inactiveFilter} onClick={() => setShowEncounter(null)}>All Cards</button>
+                    <button className={showEncounter ? styles.activeFilter : styles.inactiveFilter} onClick={() => setShowEncounter(true)}>Encounter Cards</button>
+                    <button className={showEncounter === false ? styles.activeFilter : styles.inactiveFilter} onClick={() => setShowEncounter(false)}>Hero Cards</button>
+                </div>
                 <ul className={styles.resultsList}>
-                    {results.map((card, index) => (
+                    {filteredResults.map((card, index) => (
                         <Link key={index} href={`/card/${card.Id}`}>
                             <CardImage card={card} />
                         </Link>
