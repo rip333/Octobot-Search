a7566a6aa3ac90315fc9744ba5591d671bfd9a34
Rip Britton <ripbritton3@gmail.com>
2024-03-12 09:39:55 -0500

    mobile handling for schemes

--- src/components/card-image/CardImage.tsx
+++ src/components/card-image/CardImage.tsx
@@ -16,7 +16,19 @@ const CardImage: React.FC<CardImageProps> = ({ card }) => {
       : `${imageBaseUrl}unofficial/${card.Id}.jpg`;
   };
 
-  let style = card.Type.includes("Scheme") ? { width: 515, height: 365 } : { width: 365, height: 515 };
+  // Basic mobile detection
+  const isMobile = window.innerWidth <= 768; // Assuming 'mobile' is any screen width 768px or less
+  let style = { width: 365, height: 515 }; // Default style
+
+  if (card.Type.includes("Scheme")) {
+    if (isMobile) {
+      // Adjust for mobile
+      style = { width: 365, height: 259 };
+    } else {
+      // Desktop
+      style = { width: 515, height: 365 };
+    }
+  }
 
   return (
     <div className={styles.image}>
