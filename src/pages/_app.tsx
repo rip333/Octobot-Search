d3657f9beba1a54328cd9c8219a9ae55a6348ca8
Rip Britton <ripbritton3@gmail.com>
2026-05-07 22:43:23 -0500

    new fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,20 @@ import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
+import { Manrope } from 'next/font/google';
+
+const manrope = Manrope({
+  subsets: ['latin'],
+  variable: '--font-manrope',
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <div className={`${manrope.variable} ${manrope.className} font-sans`}>
             <Component {...pageProps} />
             <Analytics />
             <SpeedInsights/>
-        </>
+        </div>
     );
 }
 
